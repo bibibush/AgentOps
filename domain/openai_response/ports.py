1210fdf3f46d2cf5529755a71272cc8bc8754cfe
jinlome <jinlome@jinlomeui-MacBookAir.local>
2026-01-14 23:14:28 +0900

    openai_api_repository 구현 완료

--- domain/openai_response/ports.py
+++ domain/openai_response/ports.py
@@ -1,11 +1,15 @@
 from typing import Protocol
 
 class OpenAIResponseAPIPort(Protocol):
-    async def create_response(self, model, input, tools, stream, **kwargs):
+    async def create_response(self, model, input, stream, **kwargs):
+        ...
+
+    async def output_response_text(self):
         ...
     
-    async def output_response(self):
+    async def output_response_json(self):
         ...
     
     async def output_response_stream(self):
         ...
+    
--- infrastructure/openai_response/api_repository.py
+++ infrastructure/openai_response/api_repository.py
@@ -6,11 +6,42 @@ class OpenAIResponseAPIRepository(OpenAIResponseAPIPort):
     def __init__(self):
         self.client = AsyncOpenAI()
         self.response = None
+        self.stream = False
 
-    async def create_response(self, model, input, tools, stream, **kwargs):
+    async def create_response(self, model, input, stream:bool,  **kwargs):
+        self.stream = stream
+        
         response = await self.client.responses.create(
             model=model,
             input=input,
+            stream=stream,
+            **kwargs
         )
 
         self.response = response
+        return response
+    
+    async def output_response_text(self):
+        if self.stream:
+            raise ValueError("Response is streamed. Use output_response_stream method instead.")
+        if self.response is None:
+            raise ValueError("No response available. Please create a response first.")
+        
+        return self.response.output_text
+    
+    async def output_response_json(self):
+        if self.stream:
+            raise ValueError("Response is streamed. Use output_response_stream method instead.")
+        if self.response is None:
+            raise ValueError("No response available. Please create a response first.")
+        
+        return self.response.output[0].to_json()
+    
+    async def output_response_stream(self):
+        if not self.stream:
+            raise ValueError("Response is not streamed. Use output_response_text or output_response_json method instead.")
+        if self.response is None:
+            raise ValueError("No response available. Please create a response first.")
+        
+        async for chunk in self.response:
+            yield chunk
